9342864e090dccc83611b6988756426dad838aeb
Rob <rob.baars@hotmail.nl>
2023-05-13 18:59:08 +0200

    comments etc

--- data/index.js
+++ data/index.js
@@ -1,15 +1,15 @@
 // Keyboard capture script
+
 let keystrokes = {};
 
-
-
-
+// flashled function for mcu
 function flashLED(times) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "/flash-led?times=" + times, true);
     xhr.send();
   }
 
+// print every key press to console and put it in a buffer for mutli key
 document.addEventListener('keydown', event => {
     if (event.key === '^') {
         console.log('^');
@@ -25,10 +25,11 @@ document.addEventListener('keydown', event => {
     if (keys.length === 2) {
         console.log(`${keys[0]} + ${keys[1]}`);
     }
-
+    flashLED(2);
     console.log(`${event.key}`);
 });
 
+// remove keypress from buffer on keyup
 document.addEventListener('keyup', event => {
     if (event.shiftKey && event.key.length === 1) {
         delete keystrokes[`${event.key.toUpperCase()}`];
--- keyboard.ino
+++ keyboard.ino
@@ -2,12 +2,18 @@
 #include <ESP8266WebServer.h>
 #include <LittleFS.h>
 
+
+
+// wifi AP mode
 const char* ssid = "keyboard";
 const char* password = "password";
 
 ESP8266WebServer server(80);
 
+// buildin led 2
 const int LED_PIN = 2;
+
+// flashled for debugging
 void flashLED(int times) {
   for (int i = 0; i < times; i++) {
     digitalWrite(LED_PIN, HIGH);
@@ -17,6 +23,7 @@ void flashLED(int times) {
   }
 }
 
+// handle webroot
 void handleRoot() {
   String clientIP = server.client().remoteIP().toString();
   Serial.print("Client connected from: ");
@@ -35,33 +42,32 @@ void setup() {
   Serial.begin(9600);
   pinMode(2, OUTPUT);
   
-
-
+  // wifi
   WiFi.softAP(ssid, password);
-
   Serial.print("AP MODE SSID:");
   Serial.println(ssid);
-
   Serial.print("SERVER IP: ");
   Serial.println(WiFi.softAPIP());
+  
 
-  server.on("/", handleRoot);
-
+  // initialize LittleFS https://github.com/littlefs-project/littlefs
   if(!LittleFS.begin()){
     Serial.println("LITTLEFS ERROR");
     return;
   }
 
-  // test c++ from javascript
+  // webserver
+  server.on("/", handleRoot);
+
+  // take flashled function javascript
   server.on("/flash-led", HTTP_GET, [](){
     String timesStr = server.arg("times");
     int times = timesStr.toInt();
     flashLED(times);
   });
 
-
+  // start webserver (alwasy after the 'on' handlers)
   server.begin();
- 
   Serial.println("INIT DONE");
 }
 

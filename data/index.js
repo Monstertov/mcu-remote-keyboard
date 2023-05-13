c0ae28951fa9c99788e8850d40d13149086c3c1e
Rob <rob.baars@hotmail.nl>
2023-05-13 18:42:40 +0200

    implement basic keydown events in javascript

---
+++ data/index.js
@@ -0,0 +1,31 @@
+// Keyboard capture script
+let keystrokes = {};
+
+document.addEventListener('keydown', event => {
+    if (event.key === '^') {
+        console.log('^');
+        return;
+    }
+    if (event.shiftKey && event.key.length === 1) {
+        keystrokes[`${event.key.toUpperCase()}`] = true;
+    } else {
+        keystrokes[`${event.key}`] = true;
+    }
+
+    const keys = Object.keys(keystrokes);
+    if (keys.length === 2) {
+        console.log(`${keys[0]} + ${keys[1]}`);
+    }
+
+    console.log(`${event.key}`);
+});
+
+document.addEventListener('keyup', event => {
+    if (event.shiftKey && event.key.length === 1) {
+        delete keystrokes[`${event.key.toUpperCase()}`];
+    } else {
+        delete keystrokes[`${event.key}`];
+    }
+
+    //console.log(`UP: ${event.key}`);
+});
